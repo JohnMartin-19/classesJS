bd43faef1b1c99e61dea8f6006d34377ece25b25
John <johnnkonge2020@gmail.com>
2023-12-24 10:29:32 +0300

    understanding basic ts syntax

--- ts.ts
+++ ts.ts
@@ -10,3 +10,18 @@ const user1:User = {
     name:"John Mburu",
     id:0,
 }
+
+//trying oop with TS 
+
+class UserAcc{
+    name:string
+    id:number;
+
+    constructor(name:string,id:number){
+        this.name = name
+        this.id = id
+    }
+}
+
+const user:User = new UserAcc('Roselyn',1)
+
