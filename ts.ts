d7861a10ac048e93018104fac8455524e5366023
John <johnnkonge2020@gmail.com>
2023-12-24 11:15:45 +0300

    used generic declaration

--- ts.ts
+++ ts.ts
@@ -25,3 +25,24 @@ class UserAcc{
 
 const user:User = new UserAcc('Roselyn',1)
 
+//using union in TS
+
+function getLength(obj:string| string[] ){
+    return obj.length
+}
+
+// generics
+interface Backpack<Type> {
+    add: (obj: Type) => void;
+    get: () => Type;
+  }
+   
+  // This line is a shortcut to tell TypeScript there is a
+  // constant called `backpack`, and to not worry about where it came from.
+  declare const backpack: Backpack<string>;
+   
+  // object is a string, because we declared it above as the variable part of Backpack.
+  const object = backpack.get();
+   
+  // Since the backpack variable is a string, you can't pass a number to the add function.
+  backpack.add('adidas');
