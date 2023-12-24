19e7a12e18c88c5125fe93932e4f4e67fce93575
John <johnnkonge2020@gmail.com>
2023-12-24 11:25:10 +0300

    structural type system

--- ts.ts
+++ ts.ts
@@ -46,3 +46,15 @@ interface Backpack<Type> {
    
   // Since the backpack variable is a string, you can't pass a number to the add function.
   backpack.add('adidas');
+  interface Point {
+    x: number;
+    y: number;
+  }
+   
+  function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+  }
+   
+  // logs "12, 26"
+  const point = { x: 12, y: 26 };
+  logPoint(point);
